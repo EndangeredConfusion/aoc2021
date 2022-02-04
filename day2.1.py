e964d259ceb12321e7fb79a81d7075826da013e5
Kaeshev_Alapati <kaeshev@gmail.com>
2022-02-04 13:21:54 -0500

    day 2.1 complete

---
+++ day2.1.py
@@ -0,0 +1,10 @@
+values = [e for e in open("C:/aocinputs/aoc2.txt", "r").read().split("\n")]
+
+print(values)
+
+horizontal = sum([int(word[-1]) for word in values if word[0] == "f"])
+up = sum(int(word[-1]) for word in values if word[0] == "u")
+down = sum(int(word[-1]) for word in values if word[0] == "d")
+
+print(abs(horizontal * (up - down)))
+
